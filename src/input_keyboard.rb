d38691bf38fbaed497dd2b97e13b961ad2ab67a4
takanobu_hamazaki <hamazakiforbis@gmail.com>
2020-02-24 17:56:13 +0900

    update

--- src/input_keyboard.rb
+++ src/input_keyboard.rb
@@ -1,29 +1,24 @@
 require 'io/console'
+@buf = Array.new
 
-class InputKeyboard
-    @buf
-    def initialize
-        @buf = Array.new
-    end
-    def init_buf
-        @buf.clear
-    end
-    
-    def wait_input
-        while (key = STDIN.getch) != "\C-c"
-            if key =~ /[\r\n]+/ 
-                if @buf.size == 5
-                    puts @buf.join.upcase
-                    init_buf
-                end
+def init_buf
+    @buf.clear
+end
+
+def wait_input
+    while (key = STDIN.getch) != "\C-c"
+        if key =~ /[\r\n]+/ 
+            if @buf.size == 5
+                puts @buf.join.upcase
+                init_buf
             end
-            
-            next unless key =~ /[[:alnum:]]/
-            if @buf.size < 5
-                print key
-                @buf.push key
-                print "[type Enter]" if @buf.size == 5
-            end
+        end
+        
+        next unless key =~ /[[:alnum:]]/
+        if @buf.size < 5
+            print key
+            @buf.push key
+            print "[type Enter]" if @buf.size == 5
         end
     end
 end
--- src/main.rb
+++ src/main.rb
@@ -2,14 +2,45 @@ require './CmdSample'
 require './CmdSample2'
 require './input_keyboard'
 
-# s = CmdSample.new /[[:alnum:]]{5}/, 0
-# s.recieve_cmd "hoge"
-# puts s.create_cmd
+s = CmdSample.new /[[:alnum:]]{5}/, 0
 # s.recieve_cmd "hoge2"
 # puts s.create_cmd
 
-# q = CmdSample2.new /[[a-z]]{1,8}/, 30
-# q.recieve_cmd "hoge"
-# puts q.create_cmd
+q = CmdSample2.new /[[a-z]]{1,8}/, 30
 # q.recieve_cmd "hoge2"
 # puts q.create_cmd
+
+# puts gather_cmd.map{|e|e.create_cmd}.join
+
+class CommandHandler
+    @cmd_list
+    def initialize
+        @cmd_list = Array.new
+    end
+    
+    def registrate cmd
+        return unless cmd.class.superclass == CommandStyle
+        @cmd_list.push cmd
+    end
+    
+    def broadcast msg
+        @cmd_list.each do |cmd|
+            cmd.recieve_cmd msg
+        end
+    end
+    
+    def create_message
+        @cmd_list.map{|cmd|cmd.create_cmd}.join
+    end
+end
+
+
+
+while msg = input_keyboard
+    ch = CommandHandler.new
+    ch.registrate s
+    ch.registrate q
+    
+    ch.broadcast "hoge"
+    puts ch.create_message
+end
