15c525e0471d75ef39221b409f2f1f8c6ec88b90
takanobu_hamazaki <hamazakiforbis@gmail.com>
2020-02-24 16:49:47 +0900

    update

--- input_keyboard.rb
+++ input_keyboard.rb
@@ -1,24 +1,29 @@
 require 'io/console'
-@buf = Array.new
 
-def init_buf
-    @buf.clear
-end
-
-def run
-    while (key = STDIN.getch) != "\C-c"
-        if key =~ /[\r\n]+/ 
-            if @buf.size == 5
-                puts @buf.join.upcase
-                init_buf
+class InputKeyboard
+    @buf
+    def initialize
+        @buf = Array.new
+    end
+    def init_buf
+        @buf.clear
+    end
+    
+    def wait_input
+        while (key = STDIN.getch) != "\C-c"
+            if key =~ /[\r\n]+/ 
+                if @buf.size == 5
+                    puts @buf.join.upcase
+                    init_buf
+                end
             end
-        end
-        
-        next unless key =~ /[[:alnum:]]/
-        if @buf.size < 5
-            print key
-            @buf.push key
-            print "[type Enter]" if @buf.size == 5
+            
+            next unless key =~ /[[:alnum:]]/
+            if @buf.size < 5
+                print key
+                @buf.push key
+                print "[type Enter]" if @buf.size == 5
+            end
         end
     end
 end
