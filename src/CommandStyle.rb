9321ecd455bc7aec0f9c966269cdfd65ace3b603
takanobu_hamazaki <hamazakiforbis@gmail.com>
2020-03-13 00:04:37 +0900

    ハッシュ案？

--- src/CommandStyle.rb
+++ src/CommandStyle.rb
@@ -24,20 +24,69 @@ class CommandStyle
 
 end
 
-syntax = [
-    {:idx => 0, :charas => 1, :type => :txt},
-    {:idx => 1, :charas => 2, :type => :num},
-    {:idx => 2, :charas => 3, :type => :txt},
+@form_tbl = [
+    {
+        :idx => 0,
+        :charas => 2,
+        :attr => {
+            :type => :txt,
+            :elm => :spd
+        }
+    },
+    {
+        :idx => 1,
+        :charas => 1,
+        :attr => {
+            :type => :num,
+            :elm => :dir
+        }
+    },
+    {
+        :idx => 2,
+        :charas => 3,
+        :attr => {
+            :type => :txt, 
+            :elm => :name
+        }
+    },
+    {
+        :idx => 3,
+        :charas => 2,
+        :attr => {
+            :type => :txt,
+            :elm => :crc
+        }
+    },
 ]
 
 
-def cmd_parse cmd_txt, syn_tbl
+def cmd_parse cmd_txt
     chunks = Array.new
     cmd_tmp = cmd_txt.split("")
-    syn_tbl.each do |stx|
-        chunks << cmd_tmp.shift(stx[:charas]).join
+    @form_tbl.each do |format|
+        val = cmd_tmp.shift(format[:charas]).join
+        chunks << [format[:idx], val, format[:attr]]
     end
     chunks
 end
 
-puts cmd_parse "123234", syntax
+def crc_check tbl
+    puts tbl.select{|e| [:crc].include?(e)}
+end
+
+def optimize tbl
+    tbl.map{|e| e[:val] = e[:val]}
+end
+def apply tbl
+    tbl.map{|e| }
+end
+
+
+msg = "123234F0"
+tbl = cmd_parse msg
+puts tbl.map{|e| e.to_s}
+puts @form_tbl
+# crc_check tbl
+# tbl = optimize tbl
+# apply tbl
+
